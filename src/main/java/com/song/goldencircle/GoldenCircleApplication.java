83ba8401ff0d7cb02c20195cbe7e880f5ab9f8d7
SMJminjeong <sdwp0920@gmail.com>
2023-06-19 21:48:07 +0900

    Initial commit

---
+++ src/main/java/com/song/goldencircle/GoldenCircleApplication.java
@@ -0,0 +1,13 @@
+package com.song.goldencircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenCircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenCircleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/song/goldencircle/GoldenCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.goldencircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenCircleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
